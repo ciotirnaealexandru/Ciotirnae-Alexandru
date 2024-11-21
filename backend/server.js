c930ae379e650888bf4dee84b5134911d5ba3612
Alexandru-Cosmin Ciotirnae <ciotirnae.alexandru005@gmail.com>
2024-11-21 22:27:09 +0200

    Add basic Frontend/Backend structure
    Signed-off-by: Alexandru-Cosmin Ciotirnae <ciotirnae.alexandru005@gmail.com>

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'OLA DE LA BACKEND' });
+});
+
+app.listen(port, () => {
+  console.log(`Serverul merge pe http://localhost:${port}`);
+});
